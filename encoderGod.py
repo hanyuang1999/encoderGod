bcc840dc4c969c594562c35555781f0805bbb373
hanyuang1999 <854688479@qq.com>
2025-10-25 03:26:45 +0800

    浴火重生大魔王

--- encoderGod.py
+++ encoderGod.py
@@ -11,12 +11,12 @@ import psutil
 
 # 这里写你的产品名，url以后换服务器会变
 url = ""
-no_key = "demo1"
+no_key = ""
 
-def post_server(machine_code):
+def verify_product_key(product_key):
     data = {
-        "product_name": no_key,
-        "machine_code": machine_code
+        "product_key": product_key,
+        "product_name": no_key
     }
     response = requests.post(url, json=data)
     return response.json()
@@ -76,14 +76,18 @@ def generate_key(machine_code):
     machine_code = machine_code + no_key
     return hashlib.md5(machine_code.encode()).hexdigest()
 
-def copy_text():
-    app.clipboard_clear()
+def endback_verify():
     machine_code = get_machine_code()
     machine_code = generate_user(machine_code)
-    app.clipboard_append(machine_code)
+    product_key = key_shop.get()
+    res = verify_product_key(product_key)
+    print(res)
+    message = res["message"]
+    key_var.set(message)
+    response_key = res["response_key"]
+    key_entry.insert(0, response_key)
+    
 
-    # button_var.set("复制成功!请将用户id粘贴发送给客服领取激活码")
-    button_var.set(f"用户id: {str(post_server(machine_code))}")
 
 def get_disk_serial_number():
     try:
@@ -130,14 +134,17 @@ if __name__ == '__main__':
     app.title("桌宠验证")
 
     key_var = tk.StringVar()
-    key_var.set("请点击下方复制按钮，粘贴出来发给客服")
+    key_var.set("请输入购买得到的激活码")
 
     key_label = tk.Label(app, textvariable=key_var)
     key_label.pack()
 
+    key_shop = tk.Entry(app)
+    key_shop.pack()
+
     button_var = tk.StringVar()
-    button_var.set("复制")
-    copy_button = tk.Button(app, textvariable=button_var, command=copy_text)
+    button_var.set("激活")
+    copy_button = tk.Button(app, textvariable=button_var, command=endback_verify)
     copy_button.pack()
 
     key_entry = tk.Entry(app)
